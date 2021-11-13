0a47301dfb241c931fb48a592d1587c49893a42e
muratcan-yuksel <muratcanyukselpro@gmail.com>
2021-11-14 02:52:09 +0300

    .

---
+++ content/home.md
@@ -0,0 +1,11 @@
+---
+title: Homepage
+description: this is a home page
+---
+
+## this is a title
+
+git add .
+git commit -am "first commit"
+git remote add git@github.com:<muratcan-yuksel>/blog.git
+git push -u origin master
---
+++ pages/index.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <h1>My blog</h1>
+    <div v-if="articles">
+      <div v-for="article in articles" :key="article.id">
+        {{ article.slug }}
+        {{ article.createdAt | formatDate }}
+        {{ article.description }}
+        <nuxt-link :to="`/blog/${article.slug}/`"> Learn more </nuxt-link>
+      </div>
+    </div>
+    <div v-else>
+      <h1>Loading...</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+import { format } from 'date-fns'
+export default {
+  name: 'Blog',
+  // data() {
+  //   return {
+  //     articles: null,
+  //   }
+  // },
+  filters: {
+    formatDate() {
+      return format(new Date(), 'dd MMM yyyy')
+    },
+  },
+  async asyncData(context) {
+    const { $content } = context
+    const articles = await $content('blog').fetch()
+    console.log(articles)
+    return { articles }
+  },
+}
+</script>
