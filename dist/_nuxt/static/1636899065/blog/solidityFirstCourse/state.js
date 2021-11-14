2329122597d6104a85812c4781d2d54052efd65b
muratcan-yuksel <muratcanyukselpro@gmail.com>
2021-11-14 21:47:49 +0300

    .

--- dist/_nuxt/static/1636899065/blog/solidityFirstCourse/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1636899065",layout:"default",error:a,serverRendered:true,routePath:"\u002Fblog\u002FsolidityFirstCourse",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"f1b51e31"}}}}(null));
