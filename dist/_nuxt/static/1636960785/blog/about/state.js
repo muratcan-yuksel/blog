c2a174bdf9dc3fab5b8cc8d69d9f97375bf71cdd
muratcan-yuksel <muratcanyukselpro@gmail.com>
2021-11-15 11:04:07 +0300

    navbar fix

--- dist/_nuxt/static/1636960785/blog/about/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1636960785",layout:"default",error:a,serverRendered:true,routePath:"\u002Fblog\u002Fabout",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"4bd49ed4"}}}}(null));
---
+++ dist/_nuxt/static/1636963399/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1636963399",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"d47beccc"}}}}(null,"\u002F"));
